61ffa265a23e096a1e2f0741d02288d1357bd3b0
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2012-02-27 21:55:49 -0500

    Use full post data in test

--- tests/tests/base.py
+++ tests/tests/base.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from testing_utils import AccountUserTestingMixin
 
 
-class BaseAccountUnauthViewTest(TestCase, AccountUserTestingMixin):
+class BaseAccountUnauthViewTest(AccountUserTestingMixin, TestCase):
     """
     Ensure all views require logged in users
     """
@@ -63,7 +63,7 @@ class BaseAccountUnauthViewTest(TestCase, AccountUserTestingMixin):
         self.assertEqual(response.status_code, 302)
 
 
-class BaseAccountViewTest(TestCase, AccountUserTestingMixin):
+class BaseAccountViewTest(AccountUserTestingMixin, TestCase):
     """
     Verify GET requests on all views. This does not cover updates via POST
     requests.
@@ -73,58 +73,68 @@ class BaseAccountViewTest(TestCase, AccountUserTestingMixin):
         self.client.login(username="user", password="pass")
 
     def test_account_list(self):
+        """Ensure that the accounts can be listed"""
         response = self.client.get(reverse("base_account_list"))
         self.assertEqual(response.status_code, 200)
 
     def test_account_add(self):
+        """Ensure that the account creation view is accessible"""
         response = self.client.get(reverse("base_account_add"))
         self.assertEqual(response.status_code, 200)
 
     def test_account_detail(self):
+        """Ensure that the account detail view is accessible"""
         response = self.client.get(reverse("base_account_detail",
             kwargs={"account_pk": self.account1.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_account_update(self):
+        """Ensure that the account update view is accessible"""
         response = self.client.get(reverse("base_account_edit",
             kwargs={"account_pk": self.account1.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_account_delete(self):
+        """Ensure that the account deletion view is accessible"""
         response = self.client.get(reverse("base_account_delete",
             kwargs={"account_pk": self.account1.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_accountuser_list(self):
+        """Ensure that the account users can be listed"""
         response = self.client.get(reverse("base_accountuser_list",
             kwargs={"account_pk": self.account1.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_accountuser_add(self):
+        """Ensure that the account user creation view is accessible"""
         response = self.client.get(reverse("base_accountuser_add",
             kwargs={"account_pk": self.account1.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_accountuser_detail(self):
+        """Ensure that the account user detail view is accessible"""
         response = self.client.get(reverse("base_accountuser_detail", kwargs={
             "account_pk": self.account1.pk,
             "accountuser_pk": self.accountuser_1.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_accountuser_update(self):
+        """Ensure that the account user update view is accessible"""
         response = self.client.get(reverse("base_accountuser_edit", kwargs={
             "account_pk": self.account1.pk,
             "accountuser_pk": self.accountuser_1.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_accountuser_delete(self):
+        """Ensure that the account user delete view is accessible"""
         response = self.client.get(reverse("base_accountuser_delete", kwargs={
             "account_pk": self.account1.pk,
             "accountuser_pk": self.accountuser_1.pk}))
         self.assertEqual(response.status_code, 200)
 
 
-class BaseCreationViewsTest(TestCase, AccountUserTestingMixin):
+class BaseCreationViewsTest(AccountUserTestingMixin, TestCase):
     """
     Test the views for creating accounts and account users
     """
@@ -133,16 +143,20 @@ class BaseCreationViewsTest(TestCase, AccountUserTestingMixin):
         self.client.login(username="user", password="pass")
 
     def test_simple_valid_account(self):
-        """Ensure that basic creation view works"""
+        """Ensure that basic account creation view works"""
         from accounts.models import Account
         start_count = Account.objects.all().count()
-        response = self.client.post(reverse("base_account_add"),
-            data={"name": "Da Bears"})
+        response = self.client.post(reverse("base_account_add"), data={
+            "account_name": "Da Bears",
+            "username": "fridge",
+            "first_name": "William",
+            "last_name": "Perry",
+            "email": "72@bears.com",})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Account.objects.all().count(), start_count + 1)
 
     def test_simple_valid_accountuser(self):
-        """Ensure that basic creation view works"""
+        """Ensure that basic account user creation view works"""
         from accounts.models import AccountUser
         start_count = AccountUser.objects.all().count()
         post_data = {
@@ -158,7 +172,7 @@ class BaseCreationViewsTest(TestCase, AccountUserTestingMixin):
         self.assertEqual(AccountUser.objects.all().count(), start_count + 1)
 
 
-class BaseDeletionViewsTest(TestCase, AccountUserTestingMixin):
+class BaseDeletionViewsTest(AccountUserTestingMixin, TestCase):
     """
     Test the views for updating accounts and account users
     """
@@ -176,7 +190,7 @@ class BaseDeletionViewsTest(TestCase, AccountUserTestingMixin):
         self.assertEqual(Account.objects.all().count(), start_count - 1)
 
 
-class BaseUpdateDeletionViewsTest(TestCase, AccountUserTestingMixin):
+class BaseUpdateDeletionViewsTest(AccountUserTestingMixin, TestCase):
     """
     Test the views for deleting accounts and account users
     """
@@ -185,14 +199,15 @@ class BaseUpdateDeletionViewsTest(TestCase, AccountUserTestingMixin):
         self.client.login(username="user", password="pass")
 
     def test_simple_account_update(self):
-        """Ensure that the account delete view deletes"""
+        """Ensure that the account update view updates"""
         response = self.client.post(reverse("base_account_edit",
-            kwargs={"account_pk": 2}),data={"pk": 2})
+            kwargs={"account_pk": 2}),data={"name": "Updated"})
         self.assertEqual(response.status_code, 302)
 
     def test_simple_accountuser_update(self):
-        """Ensure that the account delete view deletes"""
+        """Ensure that the account updates view updates"""
         response = self.client.post(reverse("base_accountuser_edit",
-            kwargs={"account_pk": 2, "accountuser_pk": 1}),data={"pk": 2})
+            kwargs={"account_pk": 2, "accountuser_pk": 1}),
+            data={"username": "newusername"})
         self.assertEqual(response.status_code, 302)
 
