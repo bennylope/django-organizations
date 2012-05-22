b9468cb7ef6bbaa41dec38a86ed123161907a832
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2012-05-22 11:48:56 -0400

    Consistently use `account_user` not `accountuser`

--- accounts/urls.py
+++ accounts/urls.py
@@ -26,23 +26,23 @@ urlpatterns = patterns('',
     # Account user URLs
     url(r'^(?P<account_pk>[\d]+)/people/$',
         view=login_required(AccountUserList.as_view()),
-        name="accountuser_list"),
+        name="account_user_list"),
     url(r'^(?P<account_pk>[\d]+)/people/add/$',
         view=login_required(AccountUserCreate.as_view()),
-        name="accountuser_add"),
+        name="account_user_add"),
     url(r'^(?P<account_pk>[\d]+)/people/(?P<account_user_pk>[\d]+)/$',
         view=login_required(AccountUserDetail.as_view()),
-        name="accountuser_detail"),
+        name="account_user_detail"),
     url(r'^(?P<account_pk>[\d]+)/people/(?P<account_user_pk>[\d]+)/edit/$',
         view=login_required(AccountUserUpdate.as_view()),
-        name="accountuser_edit"),
+        name="account_user_edit"),
     url(r'^(?P<account_pk>[\d]+)/people/(?P<account_user_pk>[\d]+)/delete/$',
         view=login_required(AccountUserDelete.as_view()),
-        name="accountuser_delete"),
+        name="account_user_delete"),
 
     # Profile
     # This view should be configurable for a custom UserProfile class
     url(r'^profile/$', view=login_required(UserProfileView.as_view()),
-        name="accountuser_profile"),
+        name="account_user_profile"),
 )
 
