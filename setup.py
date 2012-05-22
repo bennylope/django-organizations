c5b5b9175210174e665d32bda4c8bf621e5cf64d
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2012-05-21 21:21:56 -0400

    Add django-registration as dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Ben Lopatin",
     author_email="ben.lopatin@wellfireinteractive.com",
     name='django-group-accounts',
-    version='0.1a',
+    version='0.1',
     description='Group accounts for Django.',
     long_description=open(os.path.join(os.path.dirname(__file__),
         'README.rst')).read(),
@@ -24,6 +24,7 @@ setup(
     ],
     install_requires=[
         'Django>=1.3',
+        'django-registration>=0.8',
     ],
     test_suite='tests.runtests.runtests',
     include_package_data=True,
