3ad138bd26e9924dccc527e1a60fbe06eb21f023
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2012-05-27 18:32:29 -0400

    Combine manager and model tests

--- organizations/tests/__init__.py
+++ organizations/tests/__init__.py
@@ -1,3 +1,3 @@
 from .test_forms import *
-from .test_managers import *
+from .test_models import *
 from .test_utils import *
--- organizations/tests/test_models.py
+++ organizations/tests/test_models.py
@@ -1,4 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from organizations.models import Organization
 
 
+class ActiveManagerTests(TestCase):
+
+    fixtures = ['users.json', 'orgs.json']
+
+    def test_active(self):
+        self.assertEqual(3, Organization.objects.all().count())
+        self.assertEqual(2, Organization.active.all().count())
+
+    def test_by_user(self):
+        user = User.objects.get(username="dave")
+        self.assertEqual(3, Organization.objects.get_for_user(user).count())
+        self.assertEqual(2, Organization.active.get_for_user(user).count())
 
