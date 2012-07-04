8a63a08c695e59e0d3a769ac133685484fe80e82
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2012-07-04 14:40:00 -0400

    Disable debug_toolbar
    Could probably do with removing this altogether.

--- example/grouptest/settings.py
+++ example/grouptest/settings.py
@@ -136,8 +136,8 @@ INSTALLED_APPS = (
 )
 
 
-MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-INSTALLED_APPS += ('debug_toolbar',)
+#MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+#INSTALLED_APPS += ('debug_toolbar',)
 INTERNAL_IPS = ('127.0.0.1',)
 
 DEBUG_TOOLBAR_CONFIG = {
