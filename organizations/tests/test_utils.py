eba6a067dead84e25ea1ec7f308c272561481de8
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2012-07-06 14:40:45 -0400

    Fix conflicting variable names
    Using `field` in the list comprehension and as a key results in a
    KeyError due to the name conflict.

--- organizations/tests/test_utils.py
+++ organizations/tests/test_utils.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 
 from organizations.models import Organization
-from organizations.utils import create_organization
+from organizations.utils import create_organization, model_field_attr
 
 
 class OrgManagerTests(TestCase):
@@ -14,3 +14,17 @@ class OrgManagerTests(TestCase):
         acme = create_organization(user, "Acme", "acme")
         self.assertTrue(isinstance(acme, Organization))
         self.assertEqual(user, acme.owner.organization_user.user)
+
+
+class AttributeUtilTests(TestCase):
+
+    def test_present_field(self):
+        self.assertTrue(model_field_attr(User, 'username', 'max_length'))
+
+    def test_absent_field(self):
+        self.assertRaises(KeyError, model_field_attr, User, 'blahblah',
+            'max_length')
+
+    def test_absent_attr(self):
+        self.assertRaises(AttributeError, model_field_attr, User, 'username',
+            'mariopoints')
--- organizations/utils.py
+++ organizations/utils.py
@@ -16,9 +16,9 @@ def create_organization(user, name, slug, is_active=True):
     return organization
 
 
-def model_field_attr(model, field, attr):
+def model_field_attr(model, model_field, attr):
     """
     Returns the specified attribute for the specified field on the model class.
     """
     fields = dict([(field.name, field) for field in model._meta.fields])
-    return getattr(fields[field], attr)
+    return getattr(fields[model_field], attr)
