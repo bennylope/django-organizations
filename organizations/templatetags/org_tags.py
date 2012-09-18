42f82246064a2f887e168a08130e35f85e34cbd3
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2012-09-18 17:43:08 -0400

    Adds context to 'organization_users' tag

--- organizations/templatetags/org_tags.py
+++ organizations/templatetags/org_tags.py
@@ -3,6 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('organizations/organization_users.html')
-def organization_users(org):
-    return {'organization_users': org.organization_users.all()}
+@register.inclusion_tag('organizations/organization_users.html', takes_context=True)
+def organization_users(context, org):
+    context.update({'organization_users': org.organization_users.all()})
+    return context
