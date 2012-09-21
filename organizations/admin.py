4dac8b292c26dd2a03592b967d5ba34f2234c500
Paul Backhouse <paulb@aptivate.org>
2012-09-21 18:06:59 +0100

    Bug fixes. Organization instance saved so it can be refered to by dependent objects. Validation added to prevent nonsensical data.

--- organizations/admin.py
+++ organizations/admin.py
@@ -18,12 +18,14 @@ class OrganizationForm(ModelForm):
     organization_owner = forms.ModelChoiceField(queryset=User.objects.all())
     organization_users = forms.ModelMultipleChoiceField(queryset=User.objects.all(),
                                            widget=FilteredSelectMultiple(_('Users'),False,),
-                                           help_text="Select users that will be members of this organization.")
+                                           help_text="Select users that will be members of this organization.",
+                                           required=False)
     
     organization_admins = forms.ModelMultipleChoiceField(queryset=User.objects.all(),
                                            widget=FilteredSelectMultiple(_('Users'),False,),
                                            help_text="Select users that will be administrators of this organization. \
-                                           Administrators are able to add and remove users.")
+                                           Administrators are able to add and remove users.",
+                                           required=False)
 
     def __init__(self, *args, **kwargs):
         super(OrganizationForm, self).__init__(*args, **kwargs)
@@ -41,26 +43,22 @@ class OrganizationForm(ModelForm):
             pass
     
     def clean(self):
+
         cleaned_data = super(OrganizationForm, self).clean()
         organization_owner = cleaned_data.get('organization_owner')
         organization_users = set(cleaned_data.get('organization_users', ''))    
         organization_admins = set(cleaned_data.get('organization_admins', ''))
         all_users = organization_users.union(organization_admins)
-        if organization_owner not in all_users:
-            raise forms.ValidationError("%s is the owner of this group. Assign a new owner before removing this user." % self.instance.owner.organization_user.user )
+        duplicate_users = organization_users.intersection(organization_admins)
+        if organization_owner and organization_owner not in all_users:
+            raise forms.ValidationError("To own this organization %s must also be listed as either a user or an admin." % organization_owner )
+        if duplicate_users:
+            raise forms.ValidationError("A user can only be an organization user or an organization administrator, not both." )
         return cleaned_data
  
     def save(self, *args, **kwargs):
-        #save owner
-        organization_user, created = OrganizationUser.objects.get_or_create(organization=self.instance, user=self.cleaned_data['organization_owner'])
-        try:
-            organization_owner = OrganizationOwner.objects.get(organization=self.instance)
-            organization_owner.organization_user = organization_user
-        except OrganizationOwner.DoesNotExist:
-            organization_owner = OrganizationOwner(organization=self.instance, organization_user=organization_user)
-        organization_owner.save()
-        
-        #save users
+        self.instance.save()
+        #create, update or delete org users        
         form_users = set(self.cleaned_data['organization_users'])
         form_admins = set(self.cleaned_data['organization_admins'])
         form_all = form_users.union(form_admins)
@@ -71,8 +69,12 @@ class OrganizationForm(ModelForm):
         new_or_changed_users = form_users.difference(db_users)
         new_or_changed_admins = form_admins.difference(db_admins)
         
+        #delete the org user record. Also delete ownership if the user is also owner.
         for user in deleted_users:
-            OrganizationUser.objects.delete(organization=self.instance, user=user)
+            try:
+                organization_user = OrganizationUser.objects.get(organization=self.instance, user=user).delete()
+            except OrganizationUser.DoesNotExist:
+                pass
 
         for user in new_or_changed_users:
             try:
@@ -88,6 +90,23 @@ class OrganizationForm(ModelForm):
                 organization_user.save()
             except:
                 organization_user = OrganizationUser.objects.create(organization=self.instance, user=user, is_admin=True)
+
+        #create, update or delete org owner
+        user = self.cleaned_data['organization_owner']
+        if user:
+            organization_user = OrganizationUser.objects.get(organization=self.instance, user=user)
+            try:
+                organization_owner = OrganizationOwner.objects.get(organization=self.instance)
+                organization_owner.organization_user = organization_user
+            except OrganizationOwner.DoesNotExist:
+                organization_owner = OrganizationOwner(organization=self.instance, organization_user=organization_user)
+            organization_owner.save()
+        else:
+            try:
+                OrganizationOwner.objects.get(organization=self.instance).delete()
+            except OrganizationOwner.DoesNotExist:
+                pass
+
         return super(OrganizationForm, self).save(*args, **kwargs)
         
 class OrganizationAdmin(admin.ModelAdmin):
