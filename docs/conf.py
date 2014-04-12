cdba4174ad8167b9ac1f6453d9665b4d06087bf3
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-04-11 23:27:07 -0400

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-organizations'
-copyright = u'2012-2013, Ben Lopatin'
+copyright = u'2012-2014, Ben Lopatin'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
