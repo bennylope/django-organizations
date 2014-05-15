07c2cf5dcdcfa880e95e9aa8387595dab4ac07af
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-05-15 12:38:02 +0200

    Updates create_organization utility function
    Still requires updates for handling old style arguments to maintain
    backwards compatibility if feasible.

--- organizations/utils.py
+++ organizations/utils.py
@@ -1,9 +1,8 @@
-from .models import (Organization, OrganizationUser,
-        OrganizationOwner)
+from .models import Organization
 
 
-def create_organization(user, name, slug, is_active=True,
-        org_model=Organization, org_user_model=OrganizationUser):
+def create_organization(user, name,
+        org_defaults={}, org_user_defaults={}, **kwargs):
     """
     Returns a new organization, also creating an initial organization user who
     is the owner.
@@ -11,18 +10,29 @@ def create_organization(user, name, slug, is_active=True,
     The specific models can be specified if a custom organization app is used.
     The simplest way would be to use a partial.
 
-    >>> from .utils import create_organization
-    >>> from myapp.models import Account, AccountUser
+    >>> from organizations.utils import create_organization
+    >>> from myapp.models import Account
     >>> from functools import partial
-    >>> create_account = partial(create_organization,
-                org_model=Account, org_user_model=AccountUser)
+    >>> create_account = partial(create_organization, model=Account)
 
     """
-    organization = org_model.objects.create(name=name, slug=slug,
-            is_active=is_active)
-    new_user = org_user_model.objects.create(organization=organization,
-            user=user, is_admin=True)
-    OrganizationOwner.objects.create(organization=organization,
+    # TODO try to add in backwards compatability
+    org_model = kwargs.pop('model', None) or kwargs.pop('org_model', None) or Organization
+    kwargs.pop('org_user_model', None)  # Discard deprecated argument
+    org_user_klass = org_model.organization_users.related.model
+    org_owner_klass = org_model.owner.related.model
+
+    #if slug is not None:
+    #    org_defaults.update({'slug': slug})
+    #if is_active is not None:
+    #    org_defaults.update({'is_active': is_active})
+    #if is_admin is not None:
+    #    org_user_defaults.update({'is_admin': is_admin})
+
+    organization = org_model.objects.create(name=name, **org_defaults)
+    new_user = org_user_klass.objects.create(organization=organization,
+            user=user, **org_user_defaults)
+    org_owner_klass.objects.create(organization=organization,
             organization_user=new_user)
     return organization
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,53 @@
+from functools import partial
+
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.test.utils import override_settings
 
 from organizations.models import Organization
 from organizations.utils import create_organization, model_field_attr
+from test_accounts.models import Account
 
 
 @override_settings(USE_TZ=True)
-class OrgManagerTests(TestCase):
+class CreateOrgTests(TestCase):
 
     fixtures = ['users.json', 'orgs.json']
 
+    def setUp(self):
+        self.user = User.objects.get(username="dave")
+
     def test_create_organization(self):
-        user = User.objects.get(username="dave")
-        acme = create_organization(user, "Acme", "acme")
+        acme = create_organization(self.user, "Acme", org_defaults={"slug": "acme-slug"})
         self.assertTrue(isinstance(acme, Organization))
-        self.assertEqual(user, acme.owner.organization_user.user)
+        self.assertEqual(self.user, acme.owner.organization_user.user)
+
+    def test_create_custom_org(self):
+        custom = create_organization(self.user, "Custom", model=Account)
+        self.assertTrue(isinstance(custom, Account))
+        self.assertEqual(self.user, custom.owner.organization_user.user)
+
+    def test_defaults(self):
+        """Ensure models are created with defaults as specified"""
+        # Default models
+        org = create_organization(self.user, "Is Admin",
+                org_defaults={"slug": "is-admin-212", "is_active": False},
+                org_user_defaults={"is_admin": False})
+        self.assertFalse(org.is_active)
+        self.assertFalse(org.owner.organization_user.is_admin)
+
+        # Custom models
+        create_account = partial(create_organization, model=Account,
+                org_defaults={'monthly_subscription': 99},
+                org_user_defaults={'user_type': 'B'})
+        myaccount = create_account(self.user, name="My New Account")
+        self.assertEqual(myaccount.monthly_subscription, 99)
+
+    def test_backwards_compat(self):
+        """Ensure old optional arguments still work"""
+        # TODO check old style arguments
+        # org_model, org_user_model, is_admin, is_active, slug
+        pass
 
 
 class AttributeUtilTests(TestCase):
