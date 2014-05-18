d27266e021de9b6ff728f76d6691a042a0d2fb95
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-05-18 19:40:40 +0200

    Allow for installing wheels

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ except ImportError:
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
     sys.exit()
 
 readme = open('README.rst').read()
