5ff94d1ff79f307c1c01c41f33d64388970e89d9
Mauricio Antunes <mauricio.abreua@gmail.com>
2014-05-31 12:21:51 -0300

    Fixing CONTRIBUTING docs
    This PR does not just fix the docs (contributing part) but also fixes (I
    guess so) some tips about how to create the environment to develop new
    features/fix bugs. To accomplish it I changed some that were raising
    errors like 'python setup.py test' (nosetests does not run this way).
    updated setup.py and requirements.txt was replaced by
    requirements_dev.txt.
    I also took advantage to fix a type where another
    project was referenced instead of Django-organizations.

--- runtests.py
+++ runtests.py
@@ -39,7 +39,7 @@ try:
 
     from django_nose import NoseTestSuiteRunner
 except ImportError:
-    raise ImportError("To fix this error, run: pip install -r requirements-test.txt")
+    raise ImportError("To fix this error, run: pip install -r requirements_dev.txt")
 
 
 def run_tests(*test_args):
