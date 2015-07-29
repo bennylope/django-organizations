d7c74d3ecf30b92d85b9bc918c29161ff4677875
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2015-07-29 15:22:41 -0400

    Updates relationship attribute names
    The one-to-many relationship now uses a
    `ForeignRelatedObjectsDescriptor` instance
    (https://github.com/django/django/blob/adc0c4fbac98f9cb975e8fa8220323b2de638b46/django/db/models/fields/related.py#L885)
    which uses the attribute name `rel` rather than `related`.

--- organizations/utils.py
+++ organizations/utils.py
@@ -44,17 +44,13 @@ def create_organization(user, name, slug=None, is_active=None,
     org_model = kwargs.pop('model', None) or kwargs.pop('org_model', None) or Organization
     kwargs.pop('org_user_model', None)  # Discard deprecated argument
 
-    # Django 1.8 introduced the `related_model` attribute. Previously the
-    # `related.model` attribute referred to model B as pointed to by model A.
-    # In Django 1.8 that refers back to model A. Instead
-    # `related.related_model` is used to point to model B, which is what we
-    # want here.
+    org_owner_model = org_model.owner.related.related_model
     try:
-        org_user_model = org_model.organization_users.related.related_model
-        org_owner_model = org_model.owner.related.related_model
+        # Django 1.9
+        org_user_model = org_model.organization_users.rel.related_model
     except AttributeError:
-        org_user_model = org_model.organization_users.related.model
-        org_owner_model = org_model.owner.related.model
+        # Django 1.8
+        org_user_model = org_model.organization_users.related.related_model
 
     if org_defaults is None:
         org_defaults = {}
