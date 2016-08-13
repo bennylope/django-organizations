420d104d9e674b96363db5c986ea9eea4d411c92
Ben Lopatin <ben@benlopatin.com>
2016-08-13 11:30:05 -0400

    Add updated template settings to conftests

--- conftest.py
+++ conftest.py
@@ -34,5 +34,11 @@ def pytest_configure():
         FIXTURE_DIRS=['tests/fixtures'],
         ORGS_SLUGFIELD='autoslug.AutoSlugField',
         ROOT_URLCONF="tests.urls",
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'APP_DIRS': True,
+            },
+        ]
     )
     django.setup()
