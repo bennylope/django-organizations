b43143b47e6c3102ff1d6d3cbeedc5beba5dcf65
Tonye Jack <tonye@coherentpath.com>
2018-02-16 12:09:08 -0500

    Updated the migration script

--- organizations/migrations/0002_model_update.py
+++ organizations/migrations/0002_model_update.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='organization',
             name='slug',
-            field=organizations.fields.SlugField(blank=True, editable=False, help_text='The name in all lowercase, suitable for URL identification', max_length=200, populate_from=('name',), unique=True),
+            field=organizations.fields.SlugField(blank=True, editable=False, help_text='The name in all lowercase, suitable for URL identification', max_length=200, populate_from='name', unique=True),
         ),
     ]
