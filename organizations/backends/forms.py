40aa93d35fcc88974f1f81f834231886275f80cb
Ben Lopatin <ben@benlopatin.com>
2018-06-27 15:54:27 -0400

    Clean up imports

--- organizations/backends/forms.py
+++ organizations/backends/forms.py
@@ -25,8 +25,6 @@
 
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 
