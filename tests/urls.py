cc708228d29d289f6d35e481a57a47bd1427cfd8
Ben Lopatin <ben@benlopatin.com>
2018-06-26 20:48:00 -0400

    Add workable test settings
    Allow using test site as exploration site

--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from organizations.backends import invitation_backend, registration_backend
 
@@ -12,4 +13,6 @@ urlpatterns = [
     url(r"^invite/", include(invitation_backend().get_urls())),
     url(r"^register/", include(registration_backend().get_urls())),
     url(r"^accounts/", include("test_accounts.urls", namespace="test_accounts")),
-]
+] + staticfiles_urlpatterns(
+    "/static/"
+)
