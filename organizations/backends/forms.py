c642218ca881016e1e43e596ed9c0a7c9db22476
Ben Lopatin <ben@benlopatin.com>
2020-08-15 13:33:09 -0400

    Remove unused imports

--- organizations/backends/forms.py
+++ organizations/backends/forms.py
@@ -24,9 +24,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from django import forms
-from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.utils.translation import gettext_lazy as _
 
 
 class UserRegistrationForm(UserCreationForm):
